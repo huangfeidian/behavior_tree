2d1db44d7013c611b533e570da74db624a471973
huangfeidian <huangfeidian@live.cn>
2019-12-14 18:59:36 +0800

    加入图标

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 	a.setStyle(QStyleFactory::create("windowsvista"));
+	std::string icon_path = "./resources/window_icon.png";
+	a.setWindowIcon(QIcon(QString::fromStdString(icon_path)));
     MainWindow w;
     w.show();
     return a.exec();
